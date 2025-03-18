cbc38bb719c1b8131019d837788228ca309ce1d8
Paulo Junior <144707609+Pauloj2@users.noreply.github.com>
2025-03-18 13:10:41 -0300

    algoritmo de ordenação Bubble Sort
    Implementação do algoritmo bubble sort em C e C++

---
+++ bubble_sort_C.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Função para ordenar usando bubble sort
+void bubble_sort(int vetor[], int n){
+    int aux;
+    for(int i=0; i<n - 1; i++){
+        for(int j=0; j<n-i-1; j++){
+            if(vetor[j] > vetor[j+1]){
+                aux = vetor[j];
+                vetor[j] = vetor[j+1];
+                vetor[j+1] = aux;
+            }
+        }
+    }
+}
+
+int main(){
+
+    int vetor[10] = {3,5,8,1,9,2,4,7,0,6};
+    int n = 10;
+
+    printf("Vetor antes de ordenar \n");
+
+    //Imprime o vetor
+    for(int i=0; i<n; i++){
+        printf("%d ", vetor[i]);
+    }
+
+    printf("\n\n");
+
+    //Aqui e feito a chamada da função
+    bubble_sort(vetor, n);
+
+    printf("Vetor depois de ordenar com Bubble Sort \n");
+    for(int i=0; i<n; i++){
+        printf("%d ", vetor[i]);
+    }
+    printf("\n");
+
+return 0;
+}
---
+++ bubble_sort_CPP.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+//Função para ordenar usando bubble sort
+void bubbleSort(int arr[], int n){
+    int aux;
+    for(int i=0; i<n-1; i++){
+        for(int j=0; j< n-i-1; j++){
+            if(arr[j] > arr[j+1]){
+                aux = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = aux;
+            }
+        }
+    }
+
+}
+
+int main(){
+
+    int arr[10] = {40,1,23,20,67,44,9,18,0,99};
+    int n = 10;
+
+    cout << "Vetor antes de ordenar \n";
+
+    //Imprime o vetor
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n\n";
+
+    //Aqui e feito a chamada da função
+    bubbleSort(arr, n);
+
+    cout << "Vetor depois de ordenar com Bubble Sort \n";
+    for(int i=0; i<n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+
+return 0;
+}
+
